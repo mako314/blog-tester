874a95534ffdba2e4fcd3779a228b1b38ca49603
Macolister <bispo.mac@gmail.com>
2023-05-19 17:57:30 -0400

    grab the search field that we'll be using to search through our db

---
+++ index.js
@@ -0,0 +1,8 @@
+// This is where we'll globally scope some variables we'll be using. Try to remember this for the futre (hint for when we reset our forms content)
+// We console log EVERYTHING. This is the SAFEST way to ensure you're going down the right path.
+const searchBar = document.querySelector("#search-form")
+
+console.log(searchBar)
+
+//We'll grab the searchBar variable and call an eventListener on it.
+
